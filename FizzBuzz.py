93c4fce542aea2db8ff4f8330ce63e4c9b0ed173
Harry Ye <harry.ye@mail.utoronto.ca>
2024-09-08 23:21:18 -0400

    test commit from intellij

---
+++ FizzBuzz.py
@@ -0,0 +1,19 @@
+class FizzBuzz:
+
+    def main():
+        for i in range(1, 100):
+            divisible_by_3 = (i % 3 == 0)
+            divisible_by_5 = (i % 5 == 0)
+
+            if divisible_by_3 and divisible_by_5:
+                print("Fizz Buzz")
+            elif divisible_by_3:
+                print("Fizz")
+            elif divisible_by_5:
+                print("Buzz")
+            else:
+                print(i)
+
+# Run the FizzBuzz program
+if __name__ == "__main__":
+    FizzBuzz.main()
